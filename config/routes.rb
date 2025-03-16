8d00ff1617e46b43171f4ccc649f60263b8a953d
akshayrao12398 Rao <akshay.rao@kellogg.northwestern.edu>
2025-03-16 03:20:37 +0000

    Updated routes and navbar layout with proper navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
-  resources "entries"
-  resources "places"
-  resources "sessions"
-  resources "users"
+  root "places#index"
+  
+  resources :entries
+  resources :places
+  resources :users
+  
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create"
 end
